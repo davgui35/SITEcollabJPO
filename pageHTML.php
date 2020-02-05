ac12c1452aee29de4fdeb22a2dcb00d1633596ea
davgui35 <50999028+davgui35@users.noreply.github.com>
2020-02-05 18:26:20 +0100

    Update pageHTML.php

--- pageHTML.php
+++ pageHTML.php
@@ -9,7 +9,7 @@ include('includes/bootstrap.php');
 <h2>Les meilleurs articles</h2>
 <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
-    <div class="carousel-item active" data-interval="10000"> 
+    <div class="carousel-item active" data-interval="2000"> 
       <a href="#"><h3>Auteur et date</h3>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magnam voluptatibus dolor dolorum. Corrupti dicta illo omnis cumque atque totam sed iusto eos quod sapiente enim quisquam, exercitationem molestiae ipsa?</p></a>
     </div>
