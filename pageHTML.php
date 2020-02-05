a845099bc503db2e3f5b6f0681ec0f9036f004a9
davgui35 <50999028+davgui35@users.noreply.github.com>
2020-02-05 17:57:34 +0100

    Suppression titre

--- pageHTML.php
+++ pageHTML.php
@@ -6,20 +6,18 @@ include('includes/bootstrap.php');
 <?= include(DIR_TEMPLATES . 'header.php'); ?>
 
 <!-- Liste des tutos html ajouter par les utilisateurs (apercu) + lien vers une page avec l'article complet + commentaires des utilisateurs   -->
+<h2>Les meilleurs articles</h2>
 <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
-    <div class="carousel-item active" data-interval="10000">
-      <h2>Les meilleurs articles</h2>
+    <div class="carousel-item active" data-interval="10000"> 
       <a href="#"><h3>Auteur et date</h3>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magnam voluptatibus dolor dolorum. Corrupti dicta illo omnis cumque atque totam sed iusto eos quod sapiente enim quisquam, exercitationem molestiae ipsa?</p></a>
     </div>
-    <div class="carousel-item" data-interval="2000">
-    <h2>Les meilleurs articles</h2>
+    <div class="carousel-item" data-interval="2000">   
     <a href="#"><h3>Auteur et date</h3>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magnam voluptatibus dolor dolorum. Corrupti dicta illo omnis cumque atque totam sed iusto eos quod sapiente enim quisquam, exercitationem molestiae ipsa?</p></a>
     </div>
     <div class="carousel-item">
-    <h2>Les meilleurs articles</h2>
     <a href="#"><h3>Auteur et date</h3>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex magnam voluptatibus dolor dolorum. Corrupti dicta illo omnis cumque atque totam sed iusto eos quod sapiente enim quisquam, exercitationem molestiae ipsa?</p></a>
     </div>
